13c4719fb4c04be46e7cd5a59c99d67d2365c575
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-01 17:38:17 -0600

    commit 2

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -3,8 +3,25 @@
   
 // });
 
-console.log(document.getElementById('my-body').id);
+
 
 // window.onload = () => {
 //   console.log("This script loads when all the resources and DOM are ready")
+
+//   console.log(document.getElementById('my-body').id);
+
 // }
+
+// console.log("it worked")
+
+// document.cookie = "monster_name=cookie";
+document.cookie = "favorite_cookie=snickerdoodle";
+
+// // window.alert(document.cookie);
+// // alert("COOOKIEEEE");
+
+// let setCookie = (name, value) => {
+// document.cookie = `${name} = ${value}`
+// };
+
+// setCookie(100 , "choclate")
