783981153577e3abd267a8ecd100fa528a26f722
Zane Preudhomme <zane@effarig.com>
2021-02-01 16:19:30 -0700

    Worked on DOM/Page load

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -1,4 +1,10 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Your JavaScript goes here...
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   console.log("This script loads when the DOM is loaded")
   
-});
+// });
+
+console.log(document.getElementById('my-body').id);
+
+// window.onload = () => {
+//   console.log("This script loads when all the resources and DOM are ready")
+// }
