a5c72f408978e4e88ec4a1e667082044a71bfce7
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-02-01 18:13:47 -0600

    commit3

--- public/js/cookies.js
+++ public/js/cookies.js
@@ -26,16 +26,36 @@ let setCookie = (name, value) => {
 
 let getCookie = () => {
   //Set up an array
-  let array = [];
+  let array = document.cookie.split("; ")
   //Pull all of our cookies
   //push those values into the array
   //return array
-  array.push(document.cookie)
   return array;
   
 }
 
-setCookie("Hans" , "choclate");
-setCookie("Zane", "ginger molasses");
+let getCookieValue = (arg1) => {
+  let array = getCookie()
+  let length = arg1.length
+  let value = null;
+      array.forEach(element => {
+        let name = element.slice(0, length)
+          if(name === arg1){
+            value = element.slice(length + 1)
+          }
+    });
 
-console.log(getCookie());
+    return value
+}
+
+let deleteCookie = (name) => {
+  
+}
+
+
+
+setCookie("Zane", "ginger molasses");
+setCookie("Hans" , "choclate");
+
+console.log(getCookie())
+console.log(getCookieValue("Zane"));
